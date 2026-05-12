a328d9f20d8c5558e444f9c2d46eed7357278a32
PeterHsuCC <peter.c.hsu5566@gmail.com>
2026-05-11 18:17:21 -0700

    Add mock data. developer can switch between supabase and mock data in lib/core/providers.dart

--- lib/core/providers.dart
+++ lib/core/providers.dart
@@ -7,18 +7,39 @@ import 'repositories/supabase_goal_repository.dart';
 import 'repositories/supabase_task_repository.dart';
 import 'repositories/supabase_event_repository.dart';
 
+import 'repositories/mock_goal_repository.dart';
+import 'repositories/mock_task_repository.dart';
+import 'repositories/mock_event_repository.dart';
+
+const bool useMock = false;
+
 final supabaseClientProvider = Provider<SupabaseClient>((ref) {
   return Supabase.instance.client;
 });
 
 final goalRepositoryProvider = Provider<GoalRepository>((ref) {
+  if (useMock) {
+    final repo = MockGoalRepository();
+    ref.onDispose(repo.dispose);
+    return repo;
+  }
   return SupabaseGoalRepository(ref.watch(supabaseClientProvider));
 });
 
 final taskRepositoryProvider = Provider<TaskRepository>((ref) {
+  if (useMock) {
+    final repo = MockTaskRepository();
+    ref.onDispose(repo.dispose);
+    return repo;
+  }
   return SupabaseTaskRepository(ref.watch(supabaseClientProvider));
 });
 
 final eventRepositoryProvider = Provider<EventRepository>((ref) {
+  if (useMock) {
+    final repo = MockEventRepository();
+    ref.onDispose(repo.dispose);
+    return repo;
+  }
   return SupabaseEventRepository(ref.watch(supabaseClientProvider));
 });
